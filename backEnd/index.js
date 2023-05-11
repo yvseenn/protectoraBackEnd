1a7f253ee719de1f2d4b00bf8123219e0a40b076
yaseen <127450106+yvseenn@users.noreply.github.com>
2023-05-11 12:31:28 +0200

    upload

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,43 +1,68 @@
 const express = require("express");
 const cors = require("cors");
+const cloudinary=require("cloudinary").v2;
+const logger = require("morgan");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
 dotenv.config();
-const server = express();
 
-const userRoutes = require("./src/api/routes/user.routes");
-const alumnoRoutes = require("./src/api/routes/alumno.routes");
+const DB_URL = process.env.MONGO_DB;
 
-const { connect } = require("./src/config/database");
+const connect = async () => {
+  try {
+    const DB = await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const { name, host } = DB.connection;
+    console.log(`Connected to DB: ${name} in host ${host}`);
+  } catch (error) {
+    console.log("Error connecting with the DB", error);
+  }
+};
+
 connect();
 
-//SECRETKEY
-server.set("secretKey", process.env.API_SECRET);
+const PORT = process.env.PORT || 5000;
 
-//HEADERS
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+const JWT_SECRET = process.env.JWT_SECRET
 
-//JSON
+const server = express();
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-//CORS
-server.use(
-  cors({
+
+server.use(logger("dev"))
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+})
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
+server.use(cors({
     origin: "*",
-    credentials: true,
-  })
-);
+    credentials: true
+}))
 
-server.use("/users", userRoutes);
-server.use("/alumnos", alumnoRoutes);
+server.set("secretKey", JWT_SECRET)
 
-const PORT = process.env.PORT || 8000;
+// server.use("/blogs", blogsRouter)
+// server.use("/pets", petsRouter);
+// server.use("/users", userRouter);
+
+
 
 server.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
-});
+    console.log(`Server listening on http://localhost:${PORT}`)
+})
